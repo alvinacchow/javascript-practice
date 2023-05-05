d30fed0cb15e61bc3ff4ac181d6320ee1b9219ee
alvinacchow <alvinacchow@gmail.com>
2023-05-05 10:05:24 -0700

    Problem 14 Complete

--- problems/14.js
+++ problems/14.js
@@ -7,12 +7,29 @@
  * @example [3,2,3] -> 3
 */
 function problem(numbers) {
-    return null;
+    let length = numbers.length;
+    var dict = {};
+    for (let x = 0; x < length; x++) {
+        let number = numbers[x];
+        if (!(number in dict)) {
+            dict[number] = 1;
+        }
+        else {
+            dict[number] += 1;
+        }
+    }
+
+    let majority = Math.floor(length/2);
+    for (var key in dict) {
+        if (dict[key] > majority) {
+            return parseInt(key);
+        }
+
+    }
 }
 
 const tests = [
     [[3,2,3], 3],
     [[2,2,1,1,3,2,2], 2]
 ]
-
 module.exports = {problem, tests};
